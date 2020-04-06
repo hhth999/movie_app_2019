e33f5fdb49f22c77f30cedd95a8e07f0d9f5bc84
JK LEE <hhth999@naver.com>
2020-04-06 11:41:56 +0900

    0406 11:41 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import Potato from "./Potato";
 
 function App() {
   return (
     <div>
-    <h1>Hello </h1>
-    <Potato/>
+      <h1>Hello </h1>
+      <Potato />
     </div>
-    );
+  );
 }
 
 export default App;
