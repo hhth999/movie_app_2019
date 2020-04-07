4fb11d7d483f9207cc3ee4f221cad0d6783ca0cb
JK LEE <hhth999@naver.com>
2020-04-07 14:20:12 +0900

    1st Clear

--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./App.css";
+import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title}></img>
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
