479595fdf3b238692d88ef71dd3ce0f6477797b2
JK LEE <hhth999@naver.com>
2020-04-07 14:30:28 +0900

    Updates

---
+++ precache-manifest.e90b42c5fce1dbc6c823a348ba6eb467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf593ba138500d5017d7ff19466215bd",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "b658bf78e957d329bfb9",
+    "url": "/movie_app_2019/static/css/main.f06d6bbd.chunk.css"
+  },
+  {
+    "revision": "2148436f1d66bfd07f3d",
+    "url": "/movie_app_2019/static/js/2.d82b176a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.d82b176a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b658bf78e957d329bfb9",
+    "url": "/movie_app_2019/static/js/main.fd9f9196.chunk.js"
+  },
+  {
+    "revision": "cb94ca456319c7288dff",
+    "url": "/movie_app_2019/static/js/runtime-main.97eb87f9.js"
+  }
+]);
