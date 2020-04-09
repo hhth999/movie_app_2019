3114e483187e68473805773e30bde88300ca9a08
JK LEE <hhth999@naver.com>
2020-04-09 15:41:38 +0900

    Add Navigation Success

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./Movie.css";
+import "./components/Movie.css";
 
 ReactDOM.render(
   <React.StrictMode>
